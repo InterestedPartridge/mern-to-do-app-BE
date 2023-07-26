b81c203f10af25f85140e496887474f30138f19d
012_Aryan Bichpuria <bichpuriar471@gmail.com>
2023-07-27 03:43:50 +0530

    home

--- routes/user.js
+++ routes/user.js
@@ -1,13 +1,11 @@
 import express from "express";
-import { getMyProfile, login, logout, register, home } from "../controllers/user.js";
+import { getMyProfile, login, logout, register } from "../controllers/user.js";
 import { isAuthenticated } from "../middlewares/auth.js";
 
 const router = express.Router();
 
 router.post("/new", register);
 router.post("/login", login);
-router.get("/", isAuthenticated, home);
-
 
 router.get("/logout", logout);
 
